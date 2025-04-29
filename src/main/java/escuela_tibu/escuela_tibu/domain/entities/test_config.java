5d8a5c2d182ad08de76a3587c8e76789fadc4193
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-04-29 17:22:15 -0500

    chore: Remove unused import from test_config entity

--- src/main/java/escuela_tibu/escuela_tibu/domain/entities/test_config.java
+++ src/main/java/escuela_tibu/escuela_tibu/domain/entities/test_config.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
-import jakarta.websocket.Decoder.Text;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
