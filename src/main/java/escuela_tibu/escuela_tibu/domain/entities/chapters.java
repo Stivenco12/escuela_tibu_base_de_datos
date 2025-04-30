6b52165092df9f5dad7c13cc347c0fab85171897
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-04-30 16:08:52 -0500

    ✨ feat: Add updated_at field to Country entity; enhance chapters entity with new fields and relationships; introduce subchapters entity with relevant attributes and associations

--- src/main/java/escuela_tibu/escuela_tibu/domain/entities/chapters.java
+++ src/main/java/escuela_tibu/escuela_tibu/domain/entities/chapters.java
@@ -1,10 +1,16 @@
 package escuela_tibu.escuela_tibu.domain.entities;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -20,4 +26,24 @@ public class chapters {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = true, unique = true)
     int id;
+
+    @Column(name = "name_chapter")
+    int nameChapter;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", nullable = false)
+    @JsonBackReference
+    private skills skills;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "description", length = 80, nullable = true)
+    String description;
+
+    @Column(name = "chapter_summary", columnDefinition = "TEXT", nullable = true)
+    String chapterSummary;
 }
--- src/main/java/escuela_tibu/escuela_tibu/domain/entities/subchapters.java
+++ src/main/java/escuela_tibu/escuela_tibu/domain/entities/subchapters.java
@@ -1,12 +1,14 @@
 package escuela_tibu.escuela_tibu.domain.entities;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -15,21 +17,22 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
-@Table(name = "countries")
+@Table(name = "subchapters")
 @Entity
-public class Country {
+public class subchapters {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = true, unique = true)
     int id;
-    
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-    
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-    
-    @Column(name = "name_country",length = 50, nullable = false, unique = true)
-    String nameCountry;
 
+    @ManyToOne
+    @JoinColumn(name = "chapter_id", nullable = false)
+    @JsonBackReference
+    private chapters chapters;
+
+    @Column(name = "description", length = 80, nullable = true)
+    String description;
+
+    @Column(name = "summary", columnDefinition = "TEXT", nullable = true)
+    String summary;
 }
