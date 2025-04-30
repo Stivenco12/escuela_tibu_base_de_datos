cfe454f1bc6daf48e68d00c09bc3af02405479b6
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-04-30 15:24:52 -0500

    ✨ feat: Add relationships to students and test_config in exams entity

--- src/main/java/escuela_tibu/escuela_tibu/domain/entities/exams.java
+++ src/main/java/escuela_tibu/escuela_tibu/domain/entities/exams.java
@@ -2,11 +2,15 @@ package escuela_tibu.escuela_tibu.domain.entities;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -26,5 +30,13 @@ public class exams {
     @Column(columnDefinition="DATE")
     LocalDate datePresentation;
 
-    /// es una tabla de muchos a muchos completar cuando llegues a test_config
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    @JsonBackReference
+    private students students;
+
+    @ManyToOne
+    @JoinColumn(name = "config_id", nullable = false)
+    @JsonBackReference
+    private test_config test_config;
 }
